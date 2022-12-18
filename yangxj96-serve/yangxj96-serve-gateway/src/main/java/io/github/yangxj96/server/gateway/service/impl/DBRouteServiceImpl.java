e1201c250ffa19f3b4fb888a2ee0bd959036a740
yangxj96 <yangxj96@gmailc.om>
2022-12-18 18:37:11 +0800

    feat: gateway动态路由一部分

---
+++ yangxj96-serve/yangxj96-serve-gateway/src/main/java/io/github/yangxj96/server/gateway/service/impl/DBRouteServiceImpl.java
@@ -0,0 +1,70 @@
+package io.github.yangxj96.server.gateway.service.impl;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.Resource;
+import org.apache.ibatis.javassist.NotFoundException;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionRepository;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 自定义路由存储实现
+ *
+ * @author yangxj96
+ */
+@Service
+public class DBRouteServiceImpl implements RouteDefinitionRepository {
+
+    private static final String GATEWAY_REDIS_KEY = "gateway_dynamic_route";
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Resource
+    private ObjectMapper om;
+
+    @Override
+    public Flux<RouteDefinition> getRouteDefinitions() {
+        List<RouteDefinition> routes = new ArrayList<>();
+        List<Object> values = redisTemplate.opsForHash().values(GATEWAY_REDIS_KEY);
+        if (!values.isEmpty()) {
+            values.forEach(
+                    definition -> routes.add(om.convertValue(definition.toString(), RouteDefinition.class))
+            );
+            return Flux.fromIterable(routes);
+        }
+        // 此处应该从DB中获取
+        return Flux.empty();
+    }
+
+    @Override
+    public Mono<Void> save(Mono<RouteDefinition> route) {
+        return route.flatMap(definition -> {
+            try {
+                redisTemplate.opsForHash().put(GATEWAY_REDIS_KEY, definition.getId(), om.writeValueAsString(definition));
+                return Mono.empty();
+            } catch (JsonProcessingException e) {
+                // throw new RuntimeException(e)
+                return Mono.defer(() -> Mono.error(new RuntimeException("格式化异常")));
+            }
+        });
+    }
+
+    @Override
+    public Mono<Void> delete(Mono<String> routeId) {
+        return routeId.flatMap(id -> {
+            if (redisTemplate.opsForHash().hasKey(GATEWAY_REDIS_KEY, id).equals(Boolean.TRUE)) {
+                redisTemplate.opsForHash().delete(GATEWAY_REDIS_KEY, id);
+                return Mono.empty();
+            }
+            return Mono.defer(() -> Mono.error(new NotFoundException("未找到路由,id:" + id)));
+        });
+    }
+}
