09ee052ebd5a9ab87cdf6545460fdba1bf4a52b2
yangxj96 <yangxj96@gmail.com>
2022-12-19 17:27:50 +0800

    feat: update

--- yangxj96-common/src/main/java/io/github/yangxj96/common/respond/R.java
+++ yangxj96-common/src/main/java/io/github/yangxj96/common/respond/R.java
@@ -1,10 +1,13 @@
 package io.github.yangxj96.common.respond;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -31,21 +34,39 @@ public class R implements Serializable {
     private transient Object data;
 
     public static R success() {
+        setHeader(RStatus.SUCCESS);
         return R.builder().code(RStatus.SUCCESS.getCode()).msg(RStatus.SUCCESS.getMsg()).build();
     }
 
     public static R failure() {
+        setHeader(RStatus.FAILURE);
         return R.builder().code(RStatus.FAILURE.getCode()).msg(RStatus.FAILURE.getMsg()).build();
     }
 
-    public static R specify(RStatus status){
+    public static R specify(RStatus status) {
+        setHeader(status);
         return R.builder().code(status.getCode()).msg(status.getMsg()).build();
     }
 
-     public static R specify(RStatus status,Object data){
-         R r = specify(status);
-         r.setData(data);
-         return r;
+    public static R specify(RStatus status, Object data) {
+        R r = specify(status);
+        r.setData(data);
+        return r;
+    }
+
+    /**
+     * 设置请求头
+     *
+     * @param status 请求状态
+     */
+    private static void setHeader(RStatus status) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes != null) {
+            HttpServletResponse response = attributes.getResponse();
+            if (response != null) {
+                response.setIntHeader(RHttpHeadersExpand.RESULT_CODE, status.getCode());
+            }
+        }
     }
 
 }
