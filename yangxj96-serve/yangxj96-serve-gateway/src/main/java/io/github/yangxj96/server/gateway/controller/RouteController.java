ac85e41cfa69826acdee80410bfbb26ccdc4631e
yangxj96 <yangxj96@gmailc.om>
2022-12-21 00:56:08 +0800

    feat: gateway 添加route,但是还没完成

--- yangxj96-serve/yangxj96-serve-gateway/src/main/java/io/github/yangxj96/server/gateway/controller/RouteController.java
+++ yangxj96-serve/yangxj96-serve-gateway/src/main/java/io/github/yangxj96/server/gateway/controller/RouteController.java
@@ -1,15 +1,31 @@
 package io.github.yangxj96.server.gateway.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.yangxj96.bean.gateway.SysRoute;
+import io.github.yangxj96.server.gateway.service.impl.DynamicRouteServiceImpl;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.gateway.filter.FilterDefinition;
+import org.springframework.cloud.gateway.handler.predicate.PredicateDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/route")
 public class RouteController {
@@ -17,22 +33,61 @@ public class RouteController {
     @Resource
     private ObjectMapper om;
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @GetMapping("/d1")
-    public Mono<String> d1() {
+    @Resource
+    private DynamicRouteServiceImpl dynamicRouteService;
+
+
+    @PreAuthorize("hasRole('ROLE_SYSADMIN')")
+    @PostMapping("")
+    public Mono<String> create(@RequestBody SysRoute route) throws JsonProcessingException {
+        dynamicRouteService.add(assembleRouteDefinition(route));
         return Mono.just("Hello World");
     }
 
-    @PreAuthorize("hasRole('ROLE_USER1')")
-    @GetMapping("/d2")
-    public Mono<JsonNode> d2() throws JsonProcessingException {
-        String str = """
-                {
-                   "code" : 1
-                }
-                """;
-        JsonNode node = om.readTree(str);
-        return Mono.just(node);
+    private RouteDefinition assembleRouteDefinition(SysRoute route) throws JsonProcessingException {
+        RouteDefinition definition = new RouteDefinition();
+        definition.setId(route.getRouteId());
+        definition.setOrder(route.getOrder());
+
+        URI uri;
+        if (route.getUri().startsWith("http")) {
+            uri = UriComponentsBuilder.fromHttpUrl(route.getUri()).build().toUri();
+        } else {
+            uri = URI.create(route.getUri());
+        }
+        definition.setUri(uri);
+        // 设置元数据
+        if (StringUtils.isNotBlank(route.getMetadata())) {
+            Map<String, Object> metadata = om.readValue(route.getMetadata(), new TypeReference<>() {
+            });
+            definition.setMetadata(metadata);
+        }
+        // 设置断言
+        if (StringUtils.isNotBlank(route.getPredicates())) {
+            List<PredicateDefinition> predicates = new ArrayList<>();
+            for (JsonNode node : om.readTree(route.getPredicates())) {
+                PredicateDefinition datum = new PredicateDefinition();
+                datum.setArgs(om.readValue(node.get("args").asText(), new TypeReference<>() {
+                }));
+                datum.setName(node.get("name").asText());
+                predicates.add(datum);
+            }
+            definition.setPredicates(predicates);
+        }
+
+        // 设置过滤器
+        if (StringUtils.isNotBlank(route.getFilters())) {
+            List<FilterDefinition> filters = new ArrayList<>();
+            for (JsonNode node : om.readTree(route.getFilters())) {
+                FilterDefinition datum = new FilterDefinition();
+                datum.setArgs(om.readValue(node.get("args").asText(), new TypeReference<>() {
+                }));
+                datum.setName(node.get("name").asText());
+                filters.add(datum);
+            }
+            definition.setFilters(filters);
+        }
+        return definition;
     }
 
 }
