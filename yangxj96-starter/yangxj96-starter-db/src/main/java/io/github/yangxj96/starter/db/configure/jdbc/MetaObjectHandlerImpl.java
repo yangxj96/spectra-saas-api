3b648f4a972c43079653d44c51ba729a753aa255
yangxj96 <yangxj96@gmail.com>
2023-01-05 09:38:05 +0800

    feat: getCurrentUserId

--- yangxj96-starter/yangxj96-starter-db/src/main/java/io/github/yangxj96/starter/db/configure/jdbc/MetaObjectHandlerImpl.java
+++ yangxj96-starter/yangxj96-starter-db/src/main/java/io/github/yangxj96/starter/db/configure/jdbc/MetaObjectHandlerImpl.java
@@ -1,8 +1,12 @@
 package io.github.yangxj96.starter.db.configure.jdbc;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import io.github.yangxj96.bean.user.User;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.LocalDateTime;
 
@@ -29,16 +33,29 @@ public class MetaObjectHandlerImpl implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         log.debug("[自动配置-MyBatisPlus] insert 填充字段开始");
-		this.setFieldValByName(CREATED_TIME, LocalDateTime.now(), metaObject);
-		this.setFieldValByName(CREATED_BY, 0L, metaObject);
-		this.setFieldValByName(UPDATED_BY, 0L, metaObject);
-		this.setFieldValByName(UPDATED_TIME, LocalDateTime.now(), metaObject);
+        this.setFieldValByName(CREATED_BY, 0L, metaObject);
+        this.setFieldValByName(CREATED_TIME, LocalDateTime.now(), metaObject);
+        this.setFieldValByName(UPDATED_BY, getCurrentUserId(), metaObject);
+        this.setFieldValByName(UPDATED_TIME, LocalDateTime.now(), metaObject);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         log.debug("[自动配置-MyBatisPlus] update 填充字段开始");
-		this.setFieldValByName(UPDATED_TIME, LocalDateTime.now(), metaObject);
+        this.setFieldValByName(UPDATED_BY, getCurrentUserId(), metaObject);
+        this.setFieldValByName(UPDATED_TIME, LocalDateTime.now(), metaObject);
+    }
+
+    /**
+     * 获取当前用户ID
+     * @return 当前用户ID,如果没有获取到则为0
+     */
+    private Long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            return ((User) authentication.getPrincipal()).getId();
+        }
+        return 0L;
     }
 
 }
