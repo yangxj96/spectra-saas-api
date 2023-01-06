545841eb5a4cea4ec3bf6d00b48d96125b21d0a3
yangxj96 <yangxj96@gmail.com>
2023-01-07 01:00:22 +0800

    style: update

--- yangxj96-serve/yangxj96-serve-gateway/src/main/java/io/github/yangxj96/server/gateway/filters/GlobalEncryptionFilter.java
+++ yangxj96-serve/yangxj96-serve-gateway/src/main/java/io/github/yangxj96/server/gateway/filters/GlobalEncryptionFilter.java
@@ -9,9 +9,11 @@
 package io.github.yangxj96.server.gateway.filters;
 
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -30,6 +32,10 @@ public class GlobalEncryptionFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("[全局过滤器-全局解密过滤器]:进入过滤器");
+        var request = exchange.getRequest();
+        log.info("请求:{}",request.getMethod());
+        var response = exchange.getResponse();
+        log.info("响应:{}",response.getStatusCode());
         return chain.filter(exchange);
     }
 
--- yangxj96-serve/yangxj96-serve-gateway/src/main/java/io/github/yangxj96/server/gateway/service/impl/RouteDefinitionLocatorImpl.java
+++ yangxj96-serve/yangxj96-serve-gateway/src/main/java/io/github/yangxj96/server/gateway/service/impl/RouteDefinitionLocatorImpl.java
@@ -8,21 +8,21 @@
 
 package io.github.yangxj96.server.gateway.service.impl;
 
-import cn.hutool.json.JSONUtil;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import cn.hutool.extra.spring.SpringUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.yangxj96.constant.SystemRedisKey;
-import io.github.yangxj96.server.gateway.service.SysRouteService;
-import io.github.yangxj96.server.gateway.utils.RouteUtil;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.core.ReactiveHashOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,12 +41,6 @@ public class RouteDefinitionLocatorImpl implements RouteDefinitionLocator {
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
-    @Resource(name = "reactiveRedisRouteDefinitionTemplate")
-    private ReactiveRedisTemplate<String, RouteDefinition> template;
-
-    @Resource
-    private SysRouteService routeService;
-
     @Resource
     private ObjectMapper om;
 
@@ -54,29 +48,32 @@ public class RouteDefinitionLocatorImpl implements RouteDefinitionLocator {
     public Flux<RouteDefinition> getRouteDefinitions() {
         // TODO 这里需要优化
         List<RouteDefinition> routes = new ArrayList<>();
-        ReactiveHashOperations<String, Object, Object> ops = template.opsForHash();
-        List<Object> values = redisTemplate.opsForHash().values(SystemRedisKey.SYSTEM_GATEWAY_REDIS_KEY);
-        if (!values.isEmpty()) {
-            values.forEach(definition -> {
-                try {
-                    routes.add(om.convertValue(om.valueToTree(definition), RouteDefinition.class));
-                } catch (Exception e) {
-                    log.error("转换出错:" + e.getMessage());
-                    e.printStackTrace();
-                }
-            });
-        } else {
-            routeService.list(new LambdaQueryWrapper<>()).forEach(item -> {
-                RouteDefinition definition = RouteUtil.convert(item);
-                // 放到redis缓存中
-                ops.putIfAbsent(SystemRedisKey.SYSTEM_GATEWAY_REDIS_KEY, definition.getId(), JSONUtil.parse(definition).toStringPretty())
-                        .subscribe(r -> {
-                            if (r.equals(Boolean.TRUE)) {
-                                routes.add(definition);
-                            }
-                        });
-            });
+        for (Object datum : redisTemplate.opsForHash().values(SystemRedisKey.SYSTEM_GATEWAY_REDIS_KEY)) {
+            routes.add(om.convertValue(om.valueToTree(datum), RouteDefinition.class));
         }
+//        ReactiveHashOperations<String, Object, Object> ops = template.opsForHash();
+//        List<Object> values = redisTemplate.opsForHash().values(SystemRedisKey.SYSTEM_GATEWAY_REDIS_KEY);
+//        if (!values.isEmpty()) {
+//            values.forEach(definition -> {
+//                try {
+//                    routes.add(om.convertValue(om.valueToTree(definition), RouteDefinition.class));
+//                } catch (Exception e) {
+//                    log.error("转换出错:" + e.getMessage());
+//                    e.printStackTrace();
+//                }
+//            });
+//        } else {
+//            routeService.list(new LambdaQueryWrapper<>()).forEach(item -> {
+//                RouteDefinition definition = RouteUtil.convert(item);
+//                // 放到redis缓存中
+//                ops.putIfAbsent(SystemRedisKey.SYSTEM_GATEWAY_REDIS_KEY, definition.getId(), JSONUtil.parse(definition).toStringPretty())
+//                        .subscribe(r -> {
+//                            if (r.equals(Boolean.TRUE)) {
+//                                routes.add(definition);
+//                            }
+//                        });
+//            });
+//        }
         return Flux.fromIterable(routes);
     }
 }
