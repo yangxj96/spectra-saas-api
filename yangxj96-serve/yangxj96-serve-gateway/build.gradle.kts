a87e8109b448b2afc427943202852cf37fecfb12
yangxj96 <yangxj96@gmail.com>
2023-02-28 22:18:03 +0800

    style: gateway 添加 spring-cloud-starter-loadbalancer不然无法使用lb路由到其他服务

--- yangxj96-serve/yangxj96-serve-gateway/build.gradle.kts
+++ yangxj96-serve/yangxj96-serve-gateway/build.gradle.kts
@@ -16,5 +16,8 @@ dependencies {
     api(project(":yangxj96-starter:yangxj96-starter-db"))
     api(project(":yangxj96-starter:yangxj96-starter-security"))
 
+    // 网关
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    // 负载均衡
+    implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
 }
