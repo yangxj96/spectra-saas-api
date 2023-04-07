30e3a3f373cb243a7e728497dc5f5750f3d33f6a
yangxj96 <yangxj96@gmail.com>
2023-04-07 10:41:23 +0800

    feat: POST和PUT的application/json的解码处理

--- yangxj96-serve/yangxj96-serve-auth/src/main/java/io/github/yangxj96/server/auth/controller/DemoController.java
+++ yangxj96-serve/yangxj96-serve-auth/src/main/java/io/github/yangxj96/server/auth/controller/DemoController.java
@@ -8,11 +8,11 @@
 
 package io.github.yangxj96.server.auth.controller;
 
+import lombok.Data;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * demo
@@ -34,6 +34,27 @@ public class DemoController {
     @DeleteMapping
     public void delete(String id1, String id2, Integer id3) {
         log.info("id1:{},id2:{},id3:{}", id1, id2, id3);
+    }
+
+    /**
+     * 只处理json
+     */
+    @PostMapping(path = "/json", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public void post1(@RequestBody Json param) {
+        log.info("application/json:" + param.toString());
+    }
+
+    @PostMapping(path = "/fromdata", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public void post2(Json param) {
+        log.info("multipart/form-data:" + param);
+    }
+
+    @Data
+    @ToString
+    static class Json {
+        private Integer id;
+        private String name;
+        private Integer age;
     }
 
 }
