a0a90b5fc8211edd7d14f82dc1ce2cc95715ac14
yangxj96 <yangxj96@gmail.com>
2023-04-27 10:43:11 +0800

    feat: 通过注解启动动态数据源

--- yangxj96-starter/yangxj96-starter-db/src/main/java/io/github/yangxj96/starter/db/annotation/EnableDynamicDatasource.java
+++ yangxj96-starter/yangxj96-starter-db/src/main/java/io/github/yangxj96/starter/db/annotation/EnableDynamicDatasource.java
@@ -4,13 +4,16 @@ import io.github.yangxj96.starter.db.autoconfigure.DynamicDatasourceAutoConfigur
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * 动态数据源注解,主要用于确定注入动态数据源
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 @Configuration
 @Import(DynamicDatasourceAutoConfiguration.class)
 public @interface EnableDynamicDatasource {
