cc155ffc4d869372e02c336ac8d8b44441c3c3dd
yangxj96 <yangxj96@gmail.com>
2023-06-28 14:50:45 +0800

    feat: update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+import org.springframework.boot.buildpack.platform.build.PullPolicy
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
@@ -37,11 +38,6 @@ subprojects {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    // 指定中文编码
-    tasks.withType<JavaCompile> {
-        options.encoding = "UTF-8"
-    }
-
     // 定义的变量,可以再全局使用
     extra["springCloudVersion"] = "2022.0.2"
     extra["springCloudAlibabaVersion"] = "2022.0.0.0-RC1"
@@ -68,23 +64,40 @@ subprojects {
         testAnnotationProcessor("org.projectlombok:lombok")
     }
 
-    tasks.getByName<Test>("test") {
+    // 指定中文编码
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+
+    // 测试
+    tasks.named<Test>("test") {
         useJUnitPlatform()
     }
 
     // 官方提供的打包为OCI镜像的配置
     tasks.named<BootBuildImage>("bootBuildImage") {
+        // 是否发布到docker
         publish.set(false)
-        imageName.set("${project.name}:${project.version}")
+        // 镜像名称
+        // imageName.set("${project.name}:${project.version}")
+        // 不存在的时候才下载
+        pullPolicy.set(PullPolicy.IF_NOT_PRESENT)
+        // 构建器,指定版本
+        builder.set("paketobuildpacks/builder:0.1.383-tiny")
+        runImage.set("paketobuildpacks/run:1.3.128-tiny-cnb")
+
+        // buildpacks.set(listOf("gcr.io/paketo-buildpacks/amazon-corretto:latest", "urn:cnb:builder:paketo-buildpacks/java"))
+
+        // 环境变量
         environment.set(
                 mapOf(
-                        "HTTP_PROXY" to "http://192.168.2.29:8889",
-                        "HTTPS_PROXY" to "http://192.168.2.29:8889"
+                        "HTTP_PROXY" to "http://192.168.2.30:8889",
+                        "HTTPS_PROXY" to "http://192.168.2.30:8889",
+                        "BPE_DELIM_JAVA_TOOL_OPTIONS" to " ",
+                        "BPE_APPEND_JAVA_TOOL_OPTIONS" to "-Duser.timezone=Asia/Shanghai"
                 )
         )
-        docker {
-            host.set("tcp://localhost:2375")
-        }
+
         buildCache {
             volume {
                 name.set("cache-${rootProject.name}.build")
@@ -97,8 +110,14 @@ subprojects {
         }
     }
 
+    // java编译任务设置
     tasks.named("compileJava") {
         inputs.files(tasks.named("processResources"))
+    }
+
+    // java doc任务设置
+    tasks.named<Javadoc>("javadoc") {
+        options.encoding = "UTF-8"
     }
 }
 
