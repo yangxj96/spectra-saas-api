6d055475ec4df47b70ede22b5f6cec15e0749da2
Yangxj96 <yangxj96@gmail.com>
2023-08-30 19:44:37 +0800

    feat: gateway服务的stream

--- yangxj96-serve/yangxj96-serve-gateway/src/main/java/com/yangxj96/saas/server/gateway/stream/StreamConsumer.kt
+++ yangxj96-serve/yangxj96-serve-gateway/src/main/java/com/yangxj96/saas/server/gateway/stream/StreamConsumer.kt
@@ -1,8 +1,6 @@
 package com.yangxj96.saas.server.gateway.stream
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.rabbitmq.client.Channel
-import jakarta.annotation.Resource
 import org.slf4j.LoggerFactory
 import org.springframework.amqp.support.AmqpHeaders
 import org.springframework.context.annotation.Bean
@@ -21,9 +19,6 @@ import java.util.function.Consumer
 @Component
 class StreamConsumer {
 
-    @Resource
-    private lateinit var om: ObjectMapper
-
     companion object {
         private var log = LoggerFactory.getLogger(this::class.java)
     }
@@ -32,15 +27,38 @@ class StreamConsumer {
      * 租户消息
      */
     @Bean
+    fun saasTenant(): Consumer<Message<Any>> {
+        return Consumer<Message<Any>> {
+            try {
+                val channel = it.headers[AmqpHeaders.CHANNEL, Channel::class.java]
+                val delivery = it.headers[AmqpHeaders.DELIVERY_TAG, java.lang.Long::class.java]
+                val payload = it.payload
+                log.info(payload.toString())
+                if (channel != null && delivery != null) {
+                    channel.basicAck(delivery.toLong(), true)
+                } else {
+                    log.error("通道/delivery为null")
+                }
+            } catch (e: IOException) {
+                log.info("ack操作异常")
+            }
+        }
+    }
+
+
+    /**
+     * 平台通用消息
+     */
+    @Bean
     fun saasCommon(): Consumer<Message<Any>> {
         return Consumer<Message<Any>> {
             try {
                 val channel = it.headers[AmqpHeaders.CHANNEL, Channel::class.java]
-                val delivery = it.headers[AmqpHeaders.DELIVERY_TAG, Long::class.java]
+                val delivery = it.headers[AmqpHeaders.DELIVERY_TAG, java.lang.Long::class.java]
                 val payload = it.payload
                 log.info(payload.toString())
                 if (channel != null && delivery != null) {
-                    channel.basicAck(delivery, true)
+                    channel.basicAck(delivery.toLong(), true)
                 } else {
                     log.error("通道/delivery为null")
                 }
