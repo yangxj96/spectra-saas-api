9b17dcf51e20d45d5a49d542a7d0ebf698029cea
yangxj96 <yangxj96@gmail.com>
2024-03-27 15:15:08 +0800

    feat: yangxj96-starter-db 完成

---
+++ yangxj96-starter/yangxj96-starter-db/src/main/java/com/yangxj96/saas/starter/db/configure/dynamic/DynamicDataSourceRegister.java
@@ -0,0 +1,118 @@
+package com.yangxj96.saas.starter.db.configure.dynamic;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 初始化注册数据源信息
+ */
+@Getter
+public class DynamicDataSourceRegister implements EnvironmentAware {
+
+
+    /**
+     * 指定默认数据源(springboot2.0默认数据源是hikari如何想使用其他数据源可以自己配置)
+     */
+    private final Class<HikariDataSource> DATASOURCE_TYPE_DEFAULT = HikariDataSource.class;
+
+    /**
+     * 默认数据源
+     */
+    private DataSource defaultDataSource = null;
+
+    /**
+     * 用户自定义数据源
+     */
+    private Map<Object, Object> customDataSources = new HashMap<>();
+
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        // 初始化数据源注册
+        initDefaultDataSource(environment);
+        initCustomDataSources(environment);
+    }
+
+    /**
+     * 初始化自定义数据源<br></br>
+     * 在这里读取远程数据源信息后配置
+     *
+     * @param env 上下文环境
+     */
+    private void initCustomDataSources(Environment env) {
+        // 读取配置文件获取更多数据源
+        for (int i = 0; i < 1; i++) {
+            var datum = new DataSourceEntity();
+            datum.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+            datum.setUrl("jdbc:postgresql://localhost:5432/VJVDQIWBMEJSMYAJX");
+            datum.setUsername(env.getProperty("spring.datasource.username"));
+            datum.setPassword(env.getProperty("spring.datasource.password"));
+            //customDataSources["tenant-$i"] = buildDataSource(datum);
+            customDataSources.put("tenant-" + i, buildDataSource(datum));
+        }
+    }
+
+    /**
+     * 初始化默认数据源
+     *
+     * @param env 上下文环境
+     */
+    private void initDefaultDataSource(Environment env) {
+        // 读取主数据源
+        var datum = new DataSourceEntity();
+        datum.driverClassName = env.getProperty("spring.datasource.driver-class-name");
+        datum.url = env.getProperty("spring.datasource.url");
+        datum.username = env.getProperty("spring.datasource.username");
+        datum.password = env.getProperty("spring.datasource.password");
+        defaultDataSource = buildDataSource(datum);
+    }
+
+    /**
+     * 创建DataSource
+     *
+     * @param entity 数据源信息
+     * @return 创建好的数据源
+     */
+    private DataSource buildDataSource(DataSourceEntity entity) {
+        var type = entity.getType();
+        if (entity.getType() == null) {
+            entity.setType(DATASOURCE_TYPE_DEFAULT);
+        }
+
+        // 构建datasource
+        return DataSourceBuilder.create()
+                .driverClassName(entity.driverClassName)
+                .url(entity.url)
+                .username(entity.username)
+                .password(entity.password)
+                .type(type)
+                .build();
+
+    }
+
+    /**
+     * 数据源信息实体
+     */
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class DataSourceEntity {
+        private Class<HikariDataSource> type;
+        private String driverClassName;
+        private String url;
+        private String username;
+        private String password;
+    }
+
+
+}
---
+++ yangxj96-starter/yangxj96-starter-db/src/main/java/com/yangxj96/saas/starter/db/configure/jdbc/MetaObjectHandlerImpl.java
@@ -0,0 +1,71 @@
+package com.yangxj96.saas.starter.db.configure.jdbc;
+
+import cn.dev33.satoken.stp.StpUtil;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+
+import java.time.LocalDateTime;
+
+/**
+ * 元对象字段填充控制器
+ */
+@Slf4j
+public class MetaObjectHandlerImpl implements MetaObjectHandler {
+
+    /**
+     * 创建人
+     */
+    private final static String CREATED_USER = "createdUser";
+    /**
+     * 创建时间
+     */
+    private final static String CREATED_TIME = "createdTime";
+    /**
+     * 更新人
+     */
+    private final static String UPDATED_USER = "updatedUser";
+    /**
+     * 更新时间
+     */
+    private final static String UPDATED_TIME = "updatedTime";
+
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.atDebug().log("[MyBatisPlus] 新增填充字段");
+        var id = 0L;
+        try {
+            StpUtil.getLoginIdAsLong();
+        } catch (Exception e) {
+            log.atError().log("获取ID出错,默认ID为0");
+        }
+
+        if (getFieldValByName(CREATED_USER, metaObject) == null) {
+            setFieldValByName(CREATED_USER, id, metaObject);
+        }
+        if (getFieldValByName(CREATED_TIME, metaObject) == null) {
+            setFieldValByName(CREATED_TIME, LocalDateTime.now(), metaObject);
+        }
+        if (getFieldValByName(UPDATED_USER, metaObject) == null) {
+            setFieldValByName(UPDATED_USER, id, metaObject);
+        }
+        if (getFieldValByName(UPDATED_TIME, metaObject) == null) {
+            setFieldValByName(UPDATED_TIME, LocalDateTime.now(), metaObject);
+        }
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.atDebug().log("[MyBatisPlus] 修改填充字段");
+        var id = 0L;
+        try {
+            StpUtil.getLoginIdAsLong();
+        } catch (Exception e) {
+            log.atError().log("获取ID出错,默认ID为0");
+        }
+        setFieldValByName(UPDATED_USER, id, metaObject);
+        setFieldValByName(UPDATED_TIME, LocalDateTime.now(), metaObject);
+    }
+
+}
